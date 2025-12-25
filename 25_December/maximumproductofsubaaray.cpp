33d857a1ddf96203eba59c4373f05f31a8064cf5
Pradeep-gif-hub <pawasthi063@gmail.com>
2025-12-25 23:46:18 +0530

    Maximum Product subarray using Kadane algorithm similair.

---
+++ 25_December/maximumproductofsubaaray.cpp
@@ -0,0 +1,22 @@
+// keyidea-to find the maximum sum, here just keep in mind of the negative numbers
+// which can actually contribute to the larger multiples)....O(N) 
+
+class Solution{
+public:
+    int maxProduct(vector<int>& arr){
+        int n=arr.size();
+        int currentmax=arr[0];
+        int currentmin=arr[0];
+        int maxprod=arr[0];
+
+        for(int i=1;i<n;i++){
+            int temp=currentmax;
+            
+            currentmax=max(arr[i],max(currentmax*arr[i],currentmin*arr[i]));
+            currentmin=min(arr[i],min(temp*arr[i],currentmin*arr[i]));
+
+            maxprod=max(maxprod,currentmax);
+        }
+        return maxprod;
+    }
+};
